6bb80c96a66af411470707bd39c849b3a43d7e1c
shahaf <shahaf1131@gmail.com>
2022-07-03 14:01:13 +0300

    About page design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 export function About() {
-   return <main className="main-layout about">
-      <div>
+   return <main className="main-layout">
+      <div className="about">
          <h2>Abou us</h2>
          <p>Color Mania is a colorful community which trying to achieve the most loved color in the web.</p>
          <p>Join the survey and vote!</p>
+         <Link to="/">
+            Vote Here
+         </Link>
       </div>
    </main>
 }
